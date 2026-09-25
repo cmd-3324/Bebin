a725c4208624c53caf014f422debcb73cba4eab9
Max <ali.alipour4576@gmail.com>
2026-09-25 23:08:42 +0330

    :page_facing_up:

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Tests;
-
+//fghdfhgdfgdgdfgdfhertyw4564564tetbeyretyrsytyaetym679i678908708.70.fyuiui,ty
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
